044c7262fa311c2bfc735cd308ab147f04d43419
dang huu duong <dhduongt3@gmail.com>
2016-10-21 22:37:54 +0700

    Initial Commit

---
+++ KVOTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVOTest
+//
+//  Created by dang huu duong on 10/21/16.
+//  Copyright © 2016 dang huu duong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVOTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVOTest
+//
+//  Created by dang huu duong on 10/21/16.
+//  Copyright © 2016 dang huu duong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
